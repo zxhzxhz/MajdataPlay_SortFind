cdd0af380aff0227dbd15c7db5e15ab422174933
LeZi9916 <lezi@leziblog.com>
2025-04-15 10:48:02 +0800

    wip

---
+++ Assets/Scripts/Scenes/Game/Types/Extensions/SlideTableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using MajdataPlay.Game.Notes.Slide;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajdataPlay.Game.Notes
+{
+    internal static class SlideTableExtensions
+    {
+        public static SlideTable Merge(this SlideTable origin, SlideTable table2)
+        {
+            var newQueue = new SlideArea[origin.JudgeQueue.Length + table2.JudgeQueue.Length - 1];
+            var newConst = table2.Const;
+            var newName = origin.Name;
+        }
+    }
+}
